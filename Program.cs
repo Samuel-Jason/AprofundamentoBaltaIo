37981b46d4d5c71498fea8b14918a6594ba6e041
Samuel <samuelljason@gmail.com>
2023-12-21 16:48:38 -0300

    Generics Na Pratica

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using static ImersãoBalta.Program;
+
+namespace ImersãoBalta
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            var payment = new Payment();
+            var context = new DataContext<Payment>();
+            context.Save(payment);
+        }
+
+        public class DataContext<T>
+        {
+            public void Save(T entity)
+            {
+            
+            }
+        }
+
+        public class Person { }
+        public class Payment { }
+        public class Subcription { }
+    }
+}
