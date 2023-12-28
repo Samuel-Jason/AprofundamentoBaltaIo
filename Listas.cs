df8df439103b3663e4abd61df136eb3ef0ae1e80
Samuel <samuelljason@gmail.com>
2023-12-27 23:25:33 -0300

    Alguns testes em Listas, que deixei comentado em uma classe.

--- Listas.cs
+++ Listas.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using static ImersãoBalta.Program;
 
-namespace ImersãoBalta
-{
-    internal class Listas
-    {
-    }
-}
+//namespace ImersãoBalta
+//{
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var payments = new List<Payment>();
+//            payments.Add(new Payment(1));
+//            payments.Add(new Payment(2));
+//            payments.Add(new Payment(3));
+//            payments.Add(new Payment(4));
+//            payments.Add(new Payment(5));
+
+//            payments.AsEnumerable(); // o converte para IEnumerable
+//            payments.ToList(); // o converte o IEnumerable Para uma lista caso se seja IEnum
+//            payments.ToArray(); // o converte para Array
+
+//            var paidPayments = new List<Payment>();
+//            paidPayments.AddRange(payments);
+
+//            var payment = payments.Where(x => x.Id == 2).FirstOrDefault();
+//            Console.WriteLine(payment.Id);
+
+//            //foreach(var item in payments)
+//            //{
+//            //   Console.WriteLine(item.Id);                
+//            //}
+
+//            //foreach (var item in payments.Where(x => x.Id == 2))
+//            //{
+//            //    Console.WriteLine(item);
+//            //}
+//        }
+
+//        public class Payment
+//        {
+//            public int Id { get; set; }
+
+//            public Payment(int id)
+//            {
+//                Id = id;
+//            }
+//        }
+//    }
+//}
